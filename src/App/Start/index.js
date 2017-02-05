4e063aa850d5d1eeddfc71d9d114bf368b182aae
Lizhooh <960316408@qq.com>
2017-02-05 20:27:38 +0800

    add component start

---
+++ src/App/Start/index.js
@@ -0,0 +1,29 @@
+import './css/style.css';
+import React, { Component, PropTypes } from 'react';
+
+export default class Slide extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    static defaultProps = {
+        dataSource: { text: '', img: '' },
+    };
+
+    static propTypes = {
+        dataSource: PropTypes.object
+    };
+
+    render() {
+        return (
+            !this.props.hide &&
+            <div className="start-container">
+                <img className="img" src={this.props.dataSource.img} alt="" />
+                <div className="text">{this.props.dataSource.text}</div>
+            </div>
+        );
+    }
+}
