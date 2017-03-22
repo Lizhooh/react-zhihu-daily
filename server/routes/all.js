c393f73010cd5f0536773d40aa9652ea3b3f1523
Lizhooh <Lizhooh>
2017-03-22 11:11:06 +0800

    update

--- server/routes/all.js
+++ server/routes/all.js
@@ -15,33 +15,33 @@ router
     // 启动图
     .get('/start-image', async (req, res) => {
         const data = await get(API.startImage);
-        res.json(JSON.parse(data || '{ "url": "", "text": "" }'));
+        res.json(data || '{ "url": "", "text": "" }');
     })
 
     // 主题
     .get('/themes', async (req, res) => {
         const data = await get(API.themes);
-        res.json(JSON.parse(data || '{}'));
+        res.json(data || '{}');
     })
 
     // 首页
     .get('/latest', async (req, res) => {
         const data = await get(API.latest);
-        res.json(JSON.parse(data || '{}'));
+        res.json(data || '{}');
     })
 
     // 主题列表
     .get('/theme-type/:themeId', async (req, res) => {
         const id = req.params.themeId;
         const data = await get(API.themeType + id);
-        res.json(JSON.parse(data || '{}'));
+        res.json(data || '{}');
     })
 
     // 文章
     .get('/article/:articleId', async (req, res) => {
         const id = req.params.articleId;
-        const data = await get(API.article);
-        res.json(JSON.parse(data || '{}'));
+        const data = await get(API.article + id);
+        res.json(data || '{}');
     })
 
     ;
