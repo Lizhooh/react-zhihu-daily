eea6cd3f3db43b370d7f7cb4714158ba360ad5f5
Lizhooh <Lizhooh>
2017-03-22 11:06:04 +0800

    use nodejs V7.6 async, await And  Fetch.js

--- server/routes/all.js
+++ server/routes/all.js
@@ -1,68 +1,47 @@
 
-var router = require('express').Router(),
+let router = require('express').Router(),
     API = require('./module/api'),
-    $http = require('./module/$http');
+    fetch = require('node-fetch');
+
+const get = (url) => fetch(url)
+    .then(res => res.json())
+    .catch(err => console.error(err));
 
 router
-    .get('/', function (req, res, next) {
-        res.json({ result: 'ok' }).end();
+    .get('/', async (req, res) => {
+        res.json({ result: 'ok' });
     })
 
     // 启动图
-    .get('/start-image', function (req, res, next) {
-        $http.get(API.startImage, function (err, data) {
-            if (err) {
-                return void next(err);
-            }
-
-            res.json(
-                JSON.parse(data || '{ "url": "", "text": "" }')
-            ).end();
-        });
+    .get('/start-image', async (req, res) => {
+        const data = await get(API.startImage);
+        res.json(JSON.parse(data || '{ "url": "", "text": "" }'));
     })
 
     // 主题
-    .get('/themes', function (req, res, next) {
-        $http.get(API.themes, function (err, data) {
-            if (err) {
-                return void next(err);
-            }
-            res.json(JSON.parse(data || '{}')).end();
-        });
+    .get('/themes', async (req, res) => {
+        const data = await get(API.themes);
+        res.json(JSON.parse(data || '{}'));
     })
 
     // 首页
-    .get('/latest', function (req, res, next) {
-        $http.get(API.latest, function (err, data) {
-            if (err) {
-                return void next(err);
-            }
-            res.json(JSON.parse(data || '{}')).end();
-        })
+    .get('/latest', async (req, res) => {
+        const data = await get(API.latest);
+        res.json(JSON.parse(data || '{}'));
     })
 
     // 主题列表
-    .get('/theme-type/:themeId', function (req, res, next) {
-        var id = req.params.themeId;
-
-        $http.get(API.themeType + id, function (err, data) {
-            if (err) {
-                return void next(err);
-            }
-            res.json(JSON.parse(data || '{}')).end();
-        })
+    .get('/theme-type/:themeId', async (req, res) => {
+        const id = req.params.themeId;
+        const data = await get(API.themeType + id);
+        res.json(JSON.parse(data || '{}'));
     })
 
     // 文章
-    .get('/article/:articleId', function (req, res, next) {
-        var id = req.params.articleId;
-
-        $http.get(API.article + id, function (err, data) {
-            if (err) {
-                return void next(err);
-            }
-            res.json(JSON.parse(data || '{}')).end();
-        })
+    .get('/article/:articleId', async (req, res) => {
+        const id = req.params.articleId;
+        const data = await get(API.article);
+        res.json(JSON.parse(data || '{}'));
     })
 
     ;
