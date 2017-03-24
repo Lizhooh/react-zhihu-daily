46914c590abaae512dd4d83d424e52b2a747846d
Lizhooh <960316408@qq.com>
2017-03-24 15:34:54 +0800

    update router

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    browserHistory,
+    hashHistory,
     Router,
     Route,
     IndexRedirect,
@@ -11,31 +11,17 @@ import Article from '../newApp/article';
 import Popup from '../newApp/toolbar/popup';
 import Comment from '../newApp/comment';
 
-// const Hello = () => {
-//     return (
-//         <div
-//             style={{
-//                 position: 'fixed',
-//                 top: '100px',
-//                 width: '100px',
-//                 height: '100px',
-//                 left: '100px',
-//                 background: '#f66',
-//                 zIndex: 10001
-//             }}
-//             >
-//         </div>
-//     )
-// }
-
 export default () => (
-    <Router history={browserHistory} >
+    <Router history={hashHistory} >
         <Route path='/' component={null}>
             <IndexRedirect to='/themes/0' />
+
             <Route path='/themes/:id' component={Main}>
+
                 <Route path='/themes/:id/article/:aid' component={Article} >
                     <Route path='/themes/:id/article/:aid/comment/:n' component={Comment} />
                 </Route>
+
                 <Route path='/themes/:id/popup' component={Popup} />
             </Route>
         </Route>
